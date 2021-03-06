cbf97fd8112d1e49443d9962c43cc3fe65dc1d28
Arivas <33260080+Arivas00@users.noreply.github.com>
2021-03-06 11:24:31 -0500

    Adding starter code

---
+++ server.js
@@ -0,0 +1,35 @@
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+
+// Data
+
+const tables = [
+];
+
+const waitlist = [
+
+];
+
+
+// Routes
+
+app.get('/' , (req, res) => res.sendFile(path.join(__dirname, '../public/home.html')));
+
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '../public/tables.html')));
+
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, '../public/reserve.html')));
+
+app.get('/api/tables', (req, res) => res.json(tables));
+
+app.get('/api/waitlist', (req, res) => res.json(waitlist));
+
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
